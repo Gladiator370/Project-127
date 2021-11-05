192612eee197236b27e70c4e1fde175cebb44d75
Gladiator370 <75198510+Gladiator370@users.noreply.github.com>
2021-11-05 15:54:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+change_song = ""
+function preload(){
+    song = loadSound("music.mp3")
+    song = loadSound("music2.mp3")
+}
+function setup(){
+    video = createCapture(VIDEO);
+    video.hide();
+    canvas = createCanvas(600,500);
+    canvas.center();
+}
+function draw(){
+    image(video,0,0,600,500);
+}
